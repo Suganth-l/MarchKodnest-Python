dd3154ac55079ca63866fb03a9e53b1765237753
Suganth L <suganthlingappasamy@gmail.com>
2026-07-09 11:01:58 +0530

    Python Basics

--- demo.py
+++ demo.py
@@ -1,2 +1,7 @@
 print("Hello World ", end="-\n")
 print("2")
+
+name = "Suganth"
+age = "21"
+# string formatting
+print(f"My name is {name} and my age is {age}!" )
---
+++ variable.py
@@ -0,0 +1,15 @@
+a = 10
+b = "Noo"
+
+b = str(3)
+
+
+myVar = "Suganth"
+my_var = "Suganth"
+myname4 = "Suganth"
+
+
+age = 8
+age = 24
+age = age + 1
+print(age)
