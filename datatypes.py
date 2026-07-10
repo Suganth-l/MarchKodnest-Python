d26a8c12ab80e649c9ba80a55dfa2d8c14482e69
Suganth L <suganthlingappasamy@gmail.com>
2026-07-10 10:46:55 +0530

    datatypes in py

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+#str
+print('''My name is "Suganth"''')
+name = '''Suganth'''
+print(f"My name is '{name}'")
+print(type(name))
+
+#int
+a = 10
+print(a)
+a = -4
+print(a)
+
+# Float
+print(type(122.4)) 
+
+#boolean
+print(type(True))
+
